9c634cfe922d6e1e64ca09feb6df24cb51d7214a
Polina <p.echnaya@gmail.com>
2021-11-07 17:25:56 +0300

    added filtered

--- src/Table.js
+++ src/Table.js
@@ -1,31 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import "./Table.css";
 
 function Table(props) {
   const {tableData} = props;
 
+  const [ text, setText ] = useState('');
+
   const sort = () => {
-    console.log();
+
   };
 
-  const handleChange = () => () => {
-    console.log();
+  const handleChangeText = () => (e) => {
+    setText(e.target.value);
   };
+
+  const filter = () => {
+    return tableData.filter(data => {
+      const values = Object.values(data);
+      const isIncluded = values.some(value => {
+        return value.toLowerCase().includes(text.toLowerCase());
+      });
+
+      return isIncluded;
+    });
+  };
+
+  const filteredData = filter();
 
   const renderHeader = (data) => {
     const keys = Object.keys(data);
     return keys.map((key) => <td onClick={sort}>{key}</td>);
   };
+
   const renderRow = (data) => {
     const keys = Object.keys(data);
     return keys.map((key) => <td>{data[key]}</td>);
   };
+
   const renderRows = () =>{
-    return tableData.map((data) =><tr>{renderRow(data)}</tr>);
+    return filteredData.map((data) =><tr>{renderRow(data)}</tr>);
   };
 
   return (
     <div>
-      <input type="text" onChange={handleChange} />
+      <input type="text" onChange={handleChangeText()} />
       <table>
         <thead>{renderHeader(tableData[0])}</thead>
         <tbody>{renderRows()}</tbody>
